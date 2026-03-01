19da9a220cc5cfecba58da5697b40ea67770f101
Jack Couzin <3118951C@student.gla.ac.uk>
2026-03-01 21:06:58 +0000

    Chapter 7 complete. All tests pass.

--- rango/forms.py
+++ rango/forms.py
@@ -9,15 +9,14 @@ class CategoryForm(forms.ModelForm):
 
     class Meta:
         model = Category
-        fields = ('name',)
+        fields = ('name', 'views', 'likes', 'slug')
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
+        fields = ('title', 'url', 'views')
         exclude = ('category',)
 
     def clean(self):
--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True, help_text="Please enter the category name.")
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -19,8 +19,8 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
+    title = models.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = models.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = models.IntegerField(default=0)
 
     def __str__(self):
